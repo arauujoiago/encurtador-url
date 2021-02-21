386be6849c9ba4b56ff5ec155bdcb14906d4793c
Iago Galvão <arauujoiago@gmail.com>
2021-02-21 19:39:03 -0300

    Criação de regras de negocio, inserção com validação e listagem, falta fazer sistema de login e session.

--- Projeto Ramon/ProjetoDsc-main/routes/criarCadastro.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/", (req, res, next) => {
-  res.render("criarCadastro", { sessao: req.session });
-});
-
-module.exports = router;
